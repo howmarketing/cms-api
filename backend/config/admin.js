d8d84e90816952fa950d6d59e4d077558fca2501
Gabriel Ariza Gomes de Castro <gabriel@arizapp.com.br>
2022-02-13 14:22:36 -0300

    reset base

--- backend/config/admin.js
+++
@@ -1,5 +0,0 @@
-module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET', 'ac2e1cb4f955ef8daecc8295b938e4c4'),
-  },
-});
--- backend/src/api/vault-registered-user/controllers/vault-registered-user.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  vault-registered-user controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::vault-registered-user.vault-registered-user');
