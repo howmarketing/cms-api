e7b5f9ecafceb15456d61693c52629ddafa130dc
howmarketing <gabriel@arizapp.com.br>
2022-02-14 02:43:22 -0300

    last try to set all images

--- src/admin/app.example.js
+++ src/admin/app.example.js
@@ -1,12 +1,16 @@
+import AuthLogo from './extensions/my-logo.png';
+import MenuLogo from './extensions/logo.png';
+import favicon from './extensions/favicon.ico';
+
 export default {
   config: {
     locales: [
       // 'ar',
-      // 'fr',
+      'fr',
       // 'cs',
-      // 'de',
+      'de',
       // 'dk',
-      // 'es',
+      'es',
       // 'he',
       // 'id',
       // 'it',
@@ -16,8 +20,8 @@ export default {
       // 'nl',
       // 'no',
       // 'pl',
-      // 'pt-BR',
-      // 'pt',
+      'pt-BR',
+      'pt',
       // 'ru',
       // 'sk',
       // 'sv',
@@ -28,6 +32,28 @@ export default {
       // 'zh-Hans',
       // 'zh',
     ],
+    auth: {
+      logo: AuthLogo,
+    },
+   // Replace the favicon
+    head: {
+      favicon: favicon,
+    },
+    // Replace the Strapi logo in the main navigation
+    menu: {
+      logo: MenuLogo,
+    },
+    // Override or extend the theme
+    theme: {
+      colors: {
+        alternative100: '#f6ecfc',
+        alternative200: '#e0c1f4',
+        alternative500: '#ac73e6',
+        alternative600: '#9736e8',
+        alternative700: '#8312d1',
+        danger700: '#b72b1a'
+      },
+    },
   },
   bootstrap(app) {
     console.log(app);
